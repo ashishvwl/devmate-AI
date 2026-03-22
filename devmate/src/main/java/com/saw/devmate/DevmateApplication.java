3239f6420519bd64df6399c661080d529645a57c
ashish <ashishvwilson@gmail.com>
2026-03-22 13:41:34 +0530

    initial

---
+++ devmate/src/main/java/com/saw/devmate/DevmateApplication.java
@@ -0,0 +1,13 @@
+package com.saw.devmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmateApplication.class, args);
+	}
+
+}
---
+++ devmate/src/test/java/com/saw/devmate/DevmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saw.devmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
